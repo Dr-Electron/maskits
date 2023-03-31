76c075c7a4f12cd3fbc5bc9535ccb02765221105
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-04-01 00:20:31 +0200

    Implement part of frontend

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,108 @@
 #![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
 
 fn main() {
+    // launch the web app
     dioxus_web::launch(App);
 }
 
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        div { "hello, wasm!" }
-        div {
-            input { placeholder: "What needs to be done?" }
-            button { "Generate Random 24 Words" }
+        link { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" },
+        header {
+            img {
+                src: "assets/hero.svg"
+            }
         }
-        img { src: "https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png" }
+        div {
+            class: "flex flex-col items-center py-16",
+            background_color: "#9DA982",
+            div {
+                class: "sm:w-4/6",
+                div {
+                    class: "text-white px-12 mb-12",
+                    div {
+                        class: "flex font-semibold border-b-2 mb-4",
+                        border_color: "#B5C299",
+                        div {
+                            class: "px-4 py-1",
+                            background_color: "#B5C299",
+                            "STEP 1"
+                        }
+                        div {
+                            class: "flex-1 px-4 py-1",
+                            "Lorem Ipsum"
+                        }
+                    }
+                    div {
+                        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                    }
+                }
+                button {
+                    class: "bg-white font-semibold rounded-md w-full py-4",
+                    color: "#838383",
+                    "Lorem Ipsum"
+                }
+            }
+        }
+        div {
+            class: "flex flex-col items-center py-16",
+            background_color: "#5B6050",
+            div {
+                class: "sm:w-4/6",
+                div {
+                    class: "text-white px-12 mb-12",
+                    div {
+                        class: "flex font-semibold border-b-2 mb-4",
+                        border_color: "#B5C299",
+                        div {
+                            class: "px-4 py-1",
+                            background_color: "#B5C299",
+                            "STEP 2"
+                        }
+                        div {
+                            class: "flex-1 px-4 py-1",
+                            "Customize your NFT character and mint it"
+                        }
+                    }
+                    div {
+                        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                    }
+                }
+                div {
+                    class: "flex gap-4 bg-white rounded-md p-12",
+                    color: "#838383",
+                    div {
+                        class: "flex-1",
+                        img {
+                            src: "assets/avatar_bg.svg"
+                        }
+                    }
+                    div {
+                        class: "flex flex-col gap-4 flex-none w-1/4",
+                        div {
+                            class: "flex-1"
+                        }
+                        button {
+                            class: "text-white font-semibold w-full py-4 border-4",
+                            border_color: "#DE7676",
+                            background_color: "#DE7676",
+                            "RANDOMIZE"
+                        }
+                        button {
+                            class: "text-white font-semibold w-full py-4 border-4",
+                            border_color: "#A5B08D",
+                            background_color: "#B5C299",
+                            "MINT"
+                        }
+                    }
+                }
+            }
+        }
+        footer {
+            class: "h-40",
+            background_color: "#282A23"
+        }
     })
 }
